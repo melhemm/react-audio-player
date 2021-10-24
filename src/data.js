fa3c130523146db62c0586cdcd396468d635ecd7
melhemm <jamalmulhemm@gmail.com>
2021-10-25 01:12:48 +0300

    initial-commit

---
+++ src/data.js
@@ -0,0 +1,34 @@
+// import {v4 as uuidv4} from 'uuid'
+
+export const songsData = [
+  {
+    name: "KOYCE",
+    cover:
+      "https://www.ilovelimerick.ie/wp-content/uploads/2021/09/chloe-koyce-cover-3-1-scaled.jpg",
+    artist: "Aviino",
+    audio: "https://res.cloudinary.com/drddmdkvv/video/upload/v1635112357/KOYCE_-_KOYCE_gyigcz.mp3",
+    color: ["#205950", "#2ab3bf"],
+    id: 1,
+    active: true,
+  },
+  {
+    name: "Toca-Fransis-Derelle-Sharps",
+    cover:
+      "https://i.pinimg.com/originals/79/f3/f7/79f3f7025645c16e6137d8f349e8cecf.jpg",
+    artist: "Timmy-Trumpet-Kshmr",
+    audio: "https://res.cloudinary.com/drddmdkvv/video/upload/v1635112671/Carnage-feat-Timmy-Trumpet-Kshmr_-_Toca-Fransis-Derelle-Sharps-Edit_brucju.mp3",
+    color: ["#EF8EA9", "#ab417f"],
+    id: 2,
+    active: false,
+  },
+  {
+    name: "Keep Going",
+    cover:
+      "https://i1.sndcdn.com/artworks-000127331052-vt5i7u-t500x500.jpg",
+    artist: "Dimatik-Monik",
+    audio: "https://res.cloudinary.com/drddmdkvv/video/upload/v1635112679/Dimatik-Monik-Carroch_-_GIRATINA_rvajsw.mp3",
+    color: ["#CD607D", "#c94043"],
+    id: 3,
+    active: false,
+  }
+]

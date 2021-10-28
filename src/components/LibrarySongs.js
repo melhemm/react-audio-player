35c4966822c9ff95495c603ca7e82c5fbfeb5e2f
melhemm <jamalmulhemm@gmail.com>
2021-10-29 00:19:46 +0300

    event handler added

---
+++ src/components/LibrarySongs.js
@@ -0,0 +1,31 @@
+import react from 'react'
+
+const LibrarySongs = ({song, songs, setCurrentSong, id, audioRef, isPlaying}) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id)
+    setCurrentSong(selectedSong[0]);
+    // check if song is playing
+    if(isPlaying) {
+      const playPromise =  audioRef.current.play();
+      if(playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play()
+        })
+      }
+    }
+  }
+  return (
+    <div>
+      <div className="library-song" onClick={songSelectHandler}>
+        <img src={song.cover} alt={song.name} />
+        <div className="song-description">
+          <h3>{song.name}</h3>
+          <h4>{song.artist}</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LibrarySongs
+
